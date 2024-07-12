0c16cad5e6bdefc233600a05c95dda99d01ce6fc
gustavo-reinaldo <gf8370300@gmail.com>
2024-07-12 14:52:31 -0300

    -tests: teste na funcionalidade Produto e Atualizações com variáveis nos testes

---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,33 @@
+///<reference types="cypress" />
+
+describe('Funcionalidade pagina de produtos', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/');
+    });
+
+    it('Deve selecionar um produto na lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first() -> selecionar o primeiro item do product-block grid(que é a div que vai armazenar os produtos)
+            //.last() -> selecionar o ultimo item do product-block grid(que é a div que vai armazenar os produtos)
+            //.eq(3) -> selecionar o item do index do parametro utilizado no product-block grid(que é a div que vai armazenar os produtos)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click();
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+
+        var quantidade = 2
+        
+        cy.get('[class="product-block grid"]')
+        .last() //-> selecionar o primeiro item do product-block grid(que é a div que vai armazenar os produtos)
+        .click();
+
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(quantidade) //-> caso queira mudar a quantidade de itens a serem adicionados, substituir o type()
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.woocommerce-message').should('contain', quantidade)
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', (quantidade))
+    });
+});
