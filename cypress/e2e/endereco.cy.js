f461d93ce584629af81bf6f29fa335753eb068ac
gustavo-reinaldo <gf8370300@gmail.com>
2024-07-12 18:28:20 -0300

    adicionando page-objects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -8,7 +8,8 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
             cy.login(dados.user, dados.password)
         })
     });
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        EnderecoPage.editarEnderecoFaturamento()
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Dodoca', 'Pindoba', 'IA', 'Brasil', 'Rua 01 Terra Feia', 'Loteamento', 'Iguatu', 'Ceará', '63505-402', '8899241132', 'testeteste@gmail.com');
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
     });
 });
